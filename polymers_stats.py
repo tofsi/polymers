3f87527783270a1fa5c3911b9dc866b6a00e5827
Tove Nordmark <57685317+tofsi@users.noreply.github.com>
2024-01-29 13:45:17 +0100

    data!!!

--- polymers_stats.py
+++ polymers_stats.py
@@ -1,5 +1,12 @@
 from polymer import *
 from scipy import stats
+import pandas as pd
+import ast
+import re
+
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn import preprocessing
 
 # import seaborn as sns
 import matplotlib.pyplot as plt
@@ -34,19 +41,53 @@ class PolymerSample:
         self.mean_r_squared = self.r_squared.mean()
 
 
-def main():
-    # l = 1.0
-    # monomer_radius = 0.45
-    # n = 40
-    # n_draws = 10
+def save_C_n_data():
+    n_max = 65
+    n_points = 20
+    n_r_values = 20
+    n_values = pd.Series(
+        np.exp(np.linspace(1, np.log(n_max), n_points)).astype(int)
+    ).unique()
+    n_draws = 1000
+    r_values = np.linspace(0.05, 0.5, n_r_values)
 
-    # polymer_info = PolymerSample(n_draws, n, KuhnPolymer, (l, n))
-    r_squared = np.zeros(1000, dtype=float)
-    n_values = [i for i in range(1, 1001)]
-    for i, n in enumerate(n_values):
-        r_squared[i] = PolymerSample(100, KuhnPolymer(1.0, n)).mean_r_squared
-    plt.plot(n_values, r_squared, marker="o")
-    plt.show()
+    kuhn_C_n_data = pd.DataFrame(columns=["n", "r", "C_n"])
+    lattice_C_n_data = pd.DataFrame(columns=["n", "C_n"])
+    i = 0
+    print("Generating C_n Data")
+    for n in n_values:
+        for C_n in PolymerSample(n_draws * 10, SelfAvoidingLatticePolymer(n)).C_n:
+            lattice_C_n_data = pd.concat(
+                [lattice_C_n_data, pd.DataFrame([{"n": n, "C_n": C_n}])],
+                ignore_index=True,
+            )
+        for r in r_values:
+            for C_n in PolymerSample(n_draws, SelfAvoidingKuhnPolymer(1.0, n, r)).C_n:
+                kuhn_C_n_data = pd.concat(
+                    [kuhn_C_n_data, pd.DataFrame([{"n": n, "r": r, "C_n": C_n}])],
+                    ignore_index=True,
+                )
+
+            i += 1
+            if ((i * 100) / (n_points * n_r_values)) % 10 == 0:
+                print((i * 100) // (n_points * n_r_values), R"% done")
+
+    kuhn_C_n_data.to_csv("data/kuhn_C_n.csv")
+    lattice_C_n_data.to_csv("data/lattice_C_n.csv")
+
+
+def fit_C_n_data():
+    kuhn_C_n_data = pd.read_csv("data/kuhn_C_n.csv")
+    lattice_C_n_data = pd.read_csv("data/lattice_C_n.csv")
+    kuhn_C_n_data = kuhn_C_n_data.map(lambda v: np.log(v))
+    print(kuhn_C_n_data)
+
+    # kuhn_C_n_data = kuhn_C_n_data.map(lambda v: np.log(v))
+
+
+def main():
+    save_C_n_data()
+    fit_C_n_data()
 
 
 if __name__ == "__main__":
